ad7d09a2a253762a0dfa40215fb795f12280e90d
David Bainbridge <david.bainbridge@kinde.com>
2024-10-08 20:50:22 +0100

    updated text

--- kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
+++ kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
@@ -17,7 +17,7 @@ export const workflowSettings = {
   
   export default {
     async handle(event: any) {
-      kinde.accessToken.setCustomClaim("hello", "world one");
+      kinde.accessToken.setCustomClaim("hello", "awesome world");
       return "testing add user token claim";
     },
   };
