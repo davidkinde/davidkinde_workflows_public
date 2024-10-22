e3600a98701a626e929181d3c0f4b5ee168f8a53
David Bainbridge <david.bainbridge@kinde.com>
2024-10-22 14:33:03 +0100

    Add env vars to example

--- kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
+++ kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
@@ -1,4 +1,4 @@
-import { onUserTokenGeneratedEvent,  accessTokenCustomClaims, WorkflowSettings, WorkflowTrigger } from "@kinde/infrastructure"
+import { onUserTokenGeneratedEvent,  accessTokenCustomClaims, WorkflowSettings, WorkflowTrigger, getEnvironmentVariable } from "@kinde/infrastructure"
 
 export const workflowSettings: WorkflowSettings = {
   id: "addAccessTokenClaim",
@@ -13,8 +13,13 @@ export const workflowSettings: WorkflowSettings = {
 export default {
   async handle(event: onUserTokenGeneratedEvent) {
     
-    const accessToken = accessTokenCustomClaims<{ hello: string; ipAddress: string; sub: string}>();
+    const accessToken = accessTokenCustomClaims<{ key_number: string, key_string: string, key_sensitive: string, hello: string; ipAddress: string; sub: string}>();
     accessToken.hello = "Hello there!";
     accessToken.ipAddress = event.request.ipAddress
+    accessToken.key_number = getEnvironmentVariable("KEY_NUMBER")
+    accessToken.key_string = getEnvironmentVariable("KEY_STRING")
+    accessToken.key_sensitive = getEnvironmentVariable("KEY_SENSITIVE")
+
+    
   },
 };
