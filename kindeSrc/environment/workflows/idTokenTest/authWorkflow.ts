620b3be38f76505a9c04a2ec505e22041d46c4f8
David Bainbridge <david.bainbridge@kinde.com>
2024-10-22 14:50:11 +0100

    Use env var values to maintain type

--- kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
+++ kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
@@ -17,9 +17,9 @@ export default {
     const accessToken = accessTokenCustomClaims<{ key_number: number, key_string: string, key_sensitive: string, hello: string; ipAddress: string; sub: string}>();
     accessToken.hello = "Hello there!";
     accessToken.ipAddress = event.request.ipAddress
-    accessToken.key_number = getEnvironmentVariable<number>("KEY_NUMBER")
-    accessToken.key_string = getEnvironmentVariable("KEY_STRING")
-    accessToken.key_sensitive = getEnvironmentVariable("KEY_SENSITIVE")
+    accessToken.key_number = getEnvironmentVariable<number>("KEY_NUMBER").value
+    accessToken.key_string = getEnvironmentVariable("KEY_STRING").value
+    accessToken.key_sensitive = getEnvironmentVariable("KEY_SENSITIVE").value
 
     
   },
