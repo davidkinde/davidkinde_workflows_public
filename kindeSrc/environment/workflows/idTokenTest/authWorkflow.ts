5791bef19dc8a59a5c789ec1e6df9316ee72f65d
David Bainbridge <david.bainbridge@kinde.com>
2024-11-04 14:33:01 +0000

    Restore policy

--- kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
+++ kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
@@ -4,7 +4,9 @@ export const workflowSettings = {
   trigger: "user:tokens_generation",
   resetClaims: true,
   timeout: 10000,
-
+  failurePolicy:{
+    action: "continue"
+  },
   bindings: {
     console: {},
     "kinde.fetch": {},
