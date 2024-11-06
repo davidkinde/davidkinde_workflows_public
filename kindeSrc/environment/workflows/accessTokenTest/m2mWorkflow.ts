abc978dac1861ed0de702c6111d6e5813c900e2d
David Bainbridge <david.bainbridge@kinde.com>
2024-11-06 13:01:25 +0000

    Add risk and denyaccess to m2m token

--- kindeSrc/environment/workflows/accessTokenTest/m2mWorkflow.ts
+++ kindeSrc/environment/workflows/accessTokenTest/m2mWorkflow.ts
@@ -8,12 +8,30 @@ export const workflowSettings = {
     },
     bindings: {
       "kinde.m2mToken": {},
+      'kinde.risk': {},
+      'kinde.auth': {},
     },
   };
+
+  const test_data = {
+    "risk_score": 99,
+  }
+  
   
   export default {
     async handle(event: any) {
-      kinde.m2mToken.setCustomClaim("customworkflowclaim", "custom m2m claim v3");
+
+      // Setting custom claims
+      kinde.m2mToken.setCustomClaim("custom_workflow_claim_key", "custom_workflow_claim_value");
+
+      // Risk and deny access
+      kinde.risk.setScore(test_data.risk_score);
+      console.log("Risk Score", kinde.risk.getScore());
+      if (kinde.risk.getScore() > 40) {
+        kinde.auth.denyAccess('Risk score to high - access denied');
+        console.log("Denying access...", "true");
+      }
+
       return "testing m2m tokens";
     },
   };
