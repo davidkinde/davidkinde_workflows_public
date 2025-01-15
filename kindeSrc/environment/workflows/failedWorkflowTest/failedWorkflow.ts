fb722d5bd84ec7007ae7b39585e38aca8ba3e16f
David Bainbridge <david.bainbridge@kinde.com>
2025-01-15 14:07:22 +0000

    Broken workflow

--- kindeSrc/environment/workflows/failedWorkflowTest/failedWorkflow.ts
+++ kindeSrc/environment/workflows/failedWorkflowTest/failedWorkflow.ts
@@ -3,7 +3,7 @@ export const workflowSettings = {
 
 export default {
   async handle(event: any) {
-
+    ! break !
     kinde.accessToken.setCustomClaim("hello", "world");
     return "testing failed workflow";
   },
