a6f7b5fa22c332f2dc85c946133440946b7547aa
David Bainbridge <david.bainbridge@kinde.com>
2025-07-07 18:17:39 +0100

    Existing password test

---
+++ kindeSrc/environment/workflows/existingPasswordTest/existingPasswordWorkflow.ts
@@ -0,0 +1,32 @@
+import {
+  onExistingPasswordProvidedEvent,
+  WorkflowSettings,
+  WorkflowTrigger,
+  invalidateFormField,
+  secureFetch,
+  fetch,
+  getEnvironmentVariable,
+} from "@kinde/infrastructure";
+
+
+// The setting for this workflow
+export const workflowSettings: WorkflowSettings = {
+  id: "onExistingPasswordProvided",
+  trigger: WorkflowTrigger.ExistingPasswordProvided,
+  failurePolicy: {
+    action: "stop",
+  },
+  bindings: {
+    "kinde.fetch": {}
+  },
+};
+
+export default async function Workflow(event: onExistingPasswordProvidedEvent) {
+  const { hashedPassword, providedEmail, password, hasUserRecordInKinde } =
+    event.context.auth;
+  try {
+    console.log("Existing password run...")
+  } catch (error) {
+    console.error("error", error);
+  }
+}
