984d0a856fabe8ea8cc6d265cb69085c4cd0e922
David Bainbridge <david.bainbridge@kinde.com>
2025-11-06 10:47:10 +0000

    New password wf

---
+++ kindeSrc/environment/workflows/newPasswordTest/newPasswordWorkflow.ts
@@ -0,0 +1,30 @@
+import {
+  onNewPasswordProvidedEvent,
+  WorkflowSettings,
+  WorkflowTrigger,
+  invalidateFormField,
+  secureFetch,
+  fetch,
+  getEnvironmentVariable,
+} from "@kinde/infrastructure";
+
+// The setting for this workflow
+export const workflowSettings: WorkflowSettings = {
+  id: "onExistingPasswordProvided",
+  trigger: WorkflowTrigger.NewPasswordProvided,
+  failurePolicy: {
+    action: "stop",
+  },
+  bindings: {
+    "kinde.widget": {}, // Required for accessing the UI
+    "kinde.secureFetch": {}, // Required for secure external API calls
+    "kinde.env": {}, // required to access your environment variables
+    "kinde.fetch": {}, // Required for management API calls
+    url: {}, // required for url params
+  },
+};
+
+// The workflow code to be executed when the event is triggered
+export default async function Workflow(event: onNewPasswordProvidedEvent) {
+  console.log("New password provided...", JSON.stringify(event.context.user, null, 2));
+}
